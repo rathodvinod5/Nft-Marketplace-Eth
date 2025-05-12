10e242591182f4eda4063f01729c3f4db39412c7
rathodvinod5 <rathodvinod5@gmail.com>
2025-05-12 21:49:05 +0530

    changes some configs in wagmi config file

--- context/wagmiConfigContext.tsx
+++ context/wagmiConfigContext.tsx
@@ -1,7 +1,14 @@
 import { ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
 import { wagmiConfig } from "@/smart-contracts/configs/wagmiConfig";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { sepolia } from "wagmi/chains";
+import "@rainbow-me/rainbowkit/styles.css";
 
 export function WagmiConfigProviders({ children }: { children: ReactNode }) {
-  return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>;
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <RainbowKitProvider>{children}</RainbowKitProvider>
+    </WagmiProvider>
+  );
 }
