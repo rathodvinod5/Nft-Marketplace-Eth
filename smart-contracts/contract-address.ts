933c3043f5371c4cbd98bafbd4df4c92a00b0be1
rathodvinod5 <rathodvinod5@gmail.com>
2025-07-05 16:59:37 +0530

    updated abi for factory contract and new deployed address for the same

--- smart-contracts/contract-address.ts
+++ smart-contracts/contract-address.ts
@@ -1,7 +1,9 @@
 const CONTRACT_ADDRESS = {
-  factoryContractAddress: "", //"0x76b7BdF34B2289711201C994F7cf8d006D6B1470", // Sepolia testnet // "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  factoryContractAddress: "0x3DcdDeD87a3Ed1ea9392a5D0da904D49348B73f5",
   collectionContractAddress: "",
   marketplaceContractAddress: "",
 };
 
 export default CONTRACT_ADDRESS;
+
+// factory contract old: //"0x76b7BdF34B2289711201C994F7cf8d006D6B1470", // Sepolia testnet // "0x5FbDB2315678afecb367f032d93F642f64180aa3",
