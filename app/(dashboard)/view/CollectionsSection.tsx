cad704136c2ad8b709ecda96e19e93ae477526fc
rathodvinod5 <rathodvinod5@gmail.com>
2025-07-14 22:19:51 +0530

    added suspense and fallback in collections pages

--- app/(dashboard)/view/CollectionsSection.tsx
+++ app/(dashboard)/view/CollectionsSection.tsx
@@ -56,40 +56,6 @@ const CollectionsSection = () => {
           <CarouselNext className="text-gray-300 hover:text-gray-300" />
         </Carousel>
       )}
-      {/* (
-        <div className="w-full flex flex-row gap-10 overflow-x-scroll">
-          {collectionsList.map((collection, index) => {
-            return (
-              <NFTCollectionCard<CollectionsObjectType>
-                key={"collection-card-item" + index}
-                collection={collection}
-              />
-            );
-          })}
-        </div>
-      )} */}
-
-      {/* <Carousel
-        className="w-full max-w-4xl mx-auto"
-        opts={{ loop: false, align: "start" }}
-      >
-        <CarouselContent className="flex flex-row gap-4 px-3">
-          {[...Array(15)].map((_, index) => (
-            <CarouselItem key={index} className="basis-1/4 flex-none px-2">
-              <div
-                className="w-40 h-28 flex items-center justify-center text-xl font-bold 
-                background-teal-500 border border-gray-300 
-                min-w-0 basis-1/4 md:basis-1/4 lg:basis-1/4"
-              >
-                Slide {index + 1}
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-
-        <CarouselPrevious className="text-gray-300 hover:text-gray-300" />
-        <CarouselNext className="text-gray-300 hover:text-gray-300" />
-      </Carousel> */}
     </div>
   );
 };
--- app/collections/[name]/page.tsx
+++ app/collections/[name]/page.tsx
@@ -1,132 +1,126 @@
 "use client";
 
-import { use } from "react";
+import { Suspense } from "react";
 import Image from "next/image";
-import { useRouter, usePathname, useParams } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 import NFTCard from "@/components/ui/nft-card";
-import { allNFTs, collectionsList } from "@/app/(dashboard)/Data";
+import { collectionsList } from "@/app/(dashboard)/Data";
 import { CollectionsObjectType } from "@/app/(dashboard)/Types";
 import SearchAndFilterHeader from "@/components/ui/Header/SearchAndFilterHeader";
 import useCollectionsController from "../useCollectionsController";
 import { ListDisplayTypeEnum } from "../constants";
 import useCollectionTokens from "../controllers/useCollectionTokens";
+import { FiLoader } from "react-icons/fi";
 
-const AllNftFromCollectionPage = () =>
-  //   {
-  //   params,
-  // }: {
-  //   params: Promise<{ name: string }>;
-  // }
-  {
-    const router = useRouter();
-    // const { name } = use(params);
-    // const { name } = await params;
-    const params = useParams();
-    const name = params?.name;
+const AllNftFromCollectionPage = () => {
+  const router = useRouter();
+  const params = useParams();
+  const name = params?.name;
 
-    const handleSeeDetail = (nftId: number) => {
-      router.push(`/nft/${nftId}`);
-    };
+  const handleSeeDetail = (nftId: number) => {
+    router.push(`/nft/${nftId}`);
+  };
 
-    const collection: CollectionsObjectType | undefined = collectionsList.find(
-      (collection) => collection.title === name,
-    );
+  const collection: CollectionsObjectType | undefined = collectionsList.find(
+    (collection) => collection.title === name,
+  );
 
-    const {
-      allNfts,
-      filteredNfts,
-      onChangeText,
-      onFilterCategory,
-      listType,
-      onFilterListDisplayType,
-    } = useCollectionsController();
-    const items = !filteredNfts?.length ? allNfts : filteredNfts;
+  const {
+    allNfts,
+    filteredNfts,
+    onChangeText,
+    onFilterCategory,
+    listType,
+    onFilterListDisplayType,
+  } = useCollectionsController();
 
-    const collectionAddress = "0x8965DA9A5fe6f8eAB2267Adf4f5B47116E9874F5";
-    const {
-      collectionTokens,
-      isCollectionTokensLoading,
-      collectionTokensPending,
-      collectionTokensError,
-    } = useCollectionTokens(collectionAddress);
+  const items = !filteredNfts?.length ? allNfts : filteredNfts;
 
-    // console.log(
-    //   "collectionTokens",
-    //   isCollectionTokensLoading,
-    //   collectionTokensPending,
-    //   collectionTokensError,
-    //   Array(collectionTokens) &&
-    //     collectionTokens &&
-    //     collectionTokens.map((item: any) => String(item)),
-    // );
+  const collectionAddress = "0x8965DA9A5fe6f8eAB2267Adf4f5B47116E9874F5";
+  const {
+    collectionTokens,
+    isCollectionTokensLoading,
+    collectionTokensPending,
+    collectionTokensError,
+  } = useCollectionTokens(collectionAddress);
 
-    return (
-      <div className="w-full h-full px-8 py-6 bg-custom-primaryBackground">
-        <div className="flex flex-row gap-2 w-full mt-12">
-          <div className="w-1/3">
-            {collection && (
-              <Image
-                src={collection?.baseImage}
-                alt={collection?.title + "-image"}
-                width={420}
-                height={280}
-                //   apsectRatio={3/2}
-                className="aspect-auto"
-                style={{ borderRadius: "12px" }}
-              />
-            )}
+  console.log(
+    "collectionTokens",
+    isCollectionTokensLoading,
+    collectionTokensPending,
+    collectionTokensError,
+    Array.isArray(collectionTokens) &&
+      collectionTokens.map((item: any) => String(item)),
+  );
+
+  return (
+    <div className="w-full h-full px-8 py-6 bg-custom-primaryBackground">
+      <div className="flex flex-row gap-2 w-full mt-12">
+        <div className="w-1/3">
+          {collection && (
+            <Image
+              src={collection?.baseImage}
+              alt={collection?.title + "-image"}
+              width={420}
+              height={280}
+              //   apsectRatio={3/2}
+              className="aspect-auto"
+              style={{ borderRadius: "12px" }}
+            />
+          )}
+        </div>
+        <div className="w-2/3 text-gray-400 flex flex-col justify-between">
+          <div className="pt-4">
+            <p className="mb-2 text-page-title text-white">{name}</p>
+            <p>
+              Created by
+              <span className="font-semibold text-gray-300 ml-2">
+                {collection?.creator}
+              </span>
+            </p>
+            <p>
+              Address
+              <span className="font-semibold text-gray-300 ml-2">
+                {collection?.address}
+              </span>
+            </p>
           </div>
-          <div className="w-2/3 text-gray-400 flex flex-col justify-between">
-            <div className="pt-4">
-              <p className="mb-2 text-page-title text-white">{name}</p>
-              <p>
-                Created by
-                <span className="font-semibold text-gray-300 ml-2">
-                  {collection?.creator}
-                </span>
-              </p>
-              <p>
-                Address
-                <span className="font-semibold text-gray-300 ml-2">
-                  {collection?.address}
-                </span>
-              </p>
-            </div>
 
-            <div className="mt-10 flex flex-row justify-start items-center gap-12">
-              {collection?.demogs.map((demog) => (
-                <div key={demog.title} className="flex flex-col gap-1">
-                  <p className="text-gray-400">{demog.title}</p>
-                  <p className="text-gray-300 font-semibold">{demog.value}</p>
-                </div>
-              ))}
-            </div>
+          <div className="mt-10 flex flex-row justify-start items-center gap-12">
+            {collection?.demogs.map((demog) => (
+              <div key={demog.title} className="flex flex-col gap-1">
+                <p className="text-gray-400">{demog.title}</p>
+                <p className="text-gray-300 font-semibold">{demog.value}</p>
+              </div>
+            ))}
           </div>
         </div>
+      </div>
 
-        <div className="mt-20 mb-16">
-          <SearchAndFilterHeader
-            onChangeText={onChangeText}
-            itemLen={items.length}
-            onFilterCategory={onFilterCategory}
-            listType={listType}
-            onFilterListDisplayType={onFilterListDisplayType}
-            optionArray={[
-              "Football",
-              "Cricket",
-              "Tennis",
-              "Badminton",
-              "Sports",
-              "Music",
-              "Arts",
-              "Others",
-            ]}
-          />
-        </div>
+      <div className="mt-20 mb-16">
+        <SearchAndFilterHeader
+          onChangeText={onChangeText}
+          itemLen={items.length}
+          onFilterCategory={onFilterCategory}
+          listType={listType}
+          onFilterListDisplayType={onFilterListDisplayType}
+          optionArray={[
+            "Football",
+            "Cricket",
+            "Tennis",
+            "Badminton",
+            "Sports",
+            "Music",
+            "Arts",
+            "Others",
+          ]}
+        />
+      </div>
 
-        <p className="mb-8 text-xl font-semibold text-gray-200">
-          NFT's from {name}
-        </p>
+      <p className="mb-8 text-xl font-semibold text-gray-200">
+        NFT's from {name}
+      </p>
+      <Suspense fallback={<LoaderWithDiv />}>
         {listType === ListDisplayTypeEnum.LIST ? (
           <div className="flex flex-col gap-8">
             {items.map((nft) => (
@@ -150,17 +144,26 @@ const AllNftFromCollectionPage = () =>
             ))}
           </div>
         ) : null}
+      </Suspense>
 
-        <div className="w-full flex flex-row justify-center">
-          <button
-            style={{ borderRadius: "8px" }}
-            className="px-6 py-2 my-14 bg-custom-purple text-white w-fit self-center"
-          >
-            Load More
-          </button>
-        </div>
+      <div className="w-full flex flex-row justify-center">
+        <button
+          style={{ borderRadius: "8px" }}
+          className="px-6 py-2 my-14 bg-custom-purple text-white w-fit self-center"
+        >
+          Load More
+        </button>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default AllNftFromCollectionPage;
+
+const LoaderWithDiv = () => {
+  return (
+    <div className="flex justify-center items-center h-full w-full">
+      <FiLoader className="text-[2.8rem] animate-spin text-[#3B9DF8]" />
+    </div>
+  );
+};
